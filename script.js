be0128ad572d87edf4563965d6fe1db84e36d5ca
CouffignalCedric <[lecouf@hotmail.fr](mailto:lecouf@hotmail.fr)>
2022-12-02 11:21:55 +0100

    addJS

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
-// /* Sélection des éléments HTML */
-// let link = document.getElementById('link')
-// let burger = document.getElementById('burger')
-// let ul = document.querySelector('ul')
 
-// /* gestionnaire d'événement sur le a#link pour venir changer l'attribution de la classe .open à la ul
-//  et au span#burger */
-// link.addEventListener('click', function(e) {
-//   e.preventDefault()
-//   burger.classList.toggle('open')
-//   ul.classList.toggle('open')
-// })
-const imgMat= document.querySelector(".img-mat");
-let toggle= true;
-imgMat.addEventListener("click",function(){
-  toggle= !toggle;
-if(toggle){
-  imgMat.src= "assets/images/img (2).jpg";
-}else{
-  imgMat.src = "assets/images/img (3).jpg";
+  // images changent a chaque click
+  // cible les img +text 
+const imgMat1 = document.querySelector('.img1');
+const text1 = document.querySelector('.text1');
+
+const imgMat2 = document.querySelector('.img2');
+const text2 = document.querySelector('.text2');
+
+const imgMat3 = document.querySelector('.img3');
+const text3 = document.querySelector('.text3');
+
+// creation de l'evenemnt click image = image suivante
+imgMat1.addEventListener('click', function(){
   
-// }if(toggle){
-//   imgMat.src ="assets/images/img (4).jpg";
-return imgMat
-}
-})
+  imgMat2.style.height = '100px';
+  imgMat2.style.border = '4px solid';
+  
+  imgMat1.style.height = '0px';
+  imgMat1.style.border = '0px';
+      
+  text2.style.fontSize = '25px';
+      text1.style.fontSize = '0px';
+      
+});
+imgMat2.addEventListener('click', function(){
+  
+  imgMat3.style.height = '100px';
+  imgMat3.style.border = '4px solid';
+  
+  imgMat2.style.height = '0px';
+  imgMat2.style.border = '0px';
+      
+  text3.style.fontSize = '25px';
+      text2.style.fontSize = '0px';
+});
+imgMat3.addEventListener('click', function(){
+  
+  imgMat1.style.height = '250px';
+  imgMat1.style.border = '4px solid';
+  
+  imgMat3.style.height = '0px';
+  imgMat3.style.border = '0px';
+
+  imgMat2.style.height = '0px';
+  imgMat2.style.border = '0px';
+
+      
+    text1.style.fontSize = '25px';
+    text3.style.fontSize = '0px';
+    text2.style.fontSize = '0px';
+});
+
+
+
+
