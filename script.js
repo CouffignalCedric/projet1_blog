075bccc87f6b826221f3b0daa9b32890c544723b
CouffignalCedric <[lecouf@hotmail.fr](mailto:lecouf@hotmail.fr)>
2022-12-14 18:16:46 +0100

    ajout de formulaire

--- script.js
+++ script.js
@@ -2,38 +2,26 @@
 // click logo pour menu Mobile
 // cible le menu et le logo a cliquer
 const menuMobile = document.querySelector('.menuMobile');
-const logo = document.querySelector('.logoImg');
-
+const logo = document.querySelector('header');
 // creé l'evenement click
-// formule if si ecran inf a 769
-if (outerWidth < 769) {
   logo.addEventListener('click', function () {
     // creer class active
     menuMobile.classList.toggle('active');
-    // supprime le display none du css
-    // logo.classList.toggle('fixe-logo');
-
-
-  })
-}
-
-
-
+    // desactive le display none du css
+    
+})
 
 
 
 
 
 // MATERIEL images changent a chaque click
-// cible les img +text 
+// cible les img
 const imgMat1 = document.querySelector('.card1');
-// const text1 = document.querySelector('.text1');
 
 const imgMat2 = document.querySelector('.card2');
-// const text2 = document.querySelector('.text2');
 
 const imgMat3 = document.querySelector('.card3');
-// const text3 = document.querySelector('.text3');
 
 // creation de l'evenemnt click image + texte = image +texte suivant
 
@@ -42,14 +30,6 @@ imgMat1.addEventListener('click', function () {
   imgMat2.style.display = "flex"
   imgMat1.style.display = "none"
 
-  // imgMat2.style.height = '300px';
-  // imgMat2.style.border = '4px solid';
-
-  // imgMat1.style.height = '0px';
-  // imgMat1.style.border = '0px';
-
-  // text2.style.fontSize = '25px';
-  // text1.style.fontSize = '0px';
 
 });
 imgMat2.addEventListener('click', function () {
@@ -59,35 +39,51 @@ imgMat2.addEventListener('click', function () {
   imgMat2.style.display = "none"
 
 
-  // imgMat3.style.height = '300px';
-  // imgMat3.style.border = '4px solid';
-
-  // imgMat2.style.height = '0px';
-  // imgMat2.style.border = '0px';
-
-  // text3.style.fontSize = '25px';
-  // text2.style.fontSize = '0px';
 });
 imgMat3.addEventListener('click', function () {
   imgMat1.style.display = "flex"
   imgMat2.style.display = "none"
   imgMat3.style.display = "none"
 
-  // imgMat1.style.height = '300px';
-  // imgMat1.style.border = '4px solid';
-
-  // imgMat3.style.height = '0px';
-  // imgMat3.style.border = '0px';
-
-  // imgMat2.style.height = '0px';
-  // imgMat2.style.border = '0px';
-
-
-  //   text1.style.fontSize = '25px';
-  //   text3.style.fontSize = '0px';
-  //   text2.style.fontSize = '0px';
 });
 
+    // Formulaire
+
+        // cible les elements
+const form = document.querySelector("#form");
+const firstName = document.querySelector("#firstname");
+const lastName = document.querySelector("#lastname");
 
 
+
+         // utilise /onsubmit/ pour afficher message a l'envoi du formulaire
+  form.onsubmit = function (event) {
+  
+    event.preventDefault(); // evite le rechargement par default de la page par le navigateur
+    alert(`Hello ${firstName.value}, Merci pour votre message`) //message de remerciement 
+                  // le .value pour la valeur renseigner dans le formul
+    formulaire.style.display= "none"   
+      //  apres l'envoi le formulaire repasse en diplay none
+
+};
+// click contact pour afficher Formulaire
+
+        // cible le menu et le logo a cliquer
+const buttonBur = document.querySelector('.buttonCont');
+const buttonMob = document.querySelector('.lienMobCont')
+const formulaire = document.querySelector('#form');
+// const body = document.querySelector('#body')
+
+
+      // // creé l'evenement click menu Bureau
+    buttonBur.addEventListener('click', function () {
+      formulaire.style.display= "flex"
+//     // desactive le display none du css
+ })
+  // // creé l'evenement click boutton du menu mobile
+      buttonMob.addEventListener('click', function () {
+ 
+        formulaire.style.display= "flex"
+   
+     })
 
